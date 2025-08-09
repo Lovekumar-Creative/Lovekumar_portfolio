db7961b95631e93524b7891610e59381304d22c5
Lovekumar-Creative <Love.kumar13.82k@gmail.com>
2025-08-09 13:43:51 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
    // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("FyolCzJ1RpQgNvTHR");
 
         emailjs.sendForm('service_3bovcm9', 'template_0eru9p9', '#contact-form')
             .then(function (response) {
